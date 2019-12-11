06093342a53c751aece9438cb78ede1a4de24ea6
kraemermar <kraemermar@onkraemer.de@ip-192-168-13-107>
2019-12-11 11:39:20 +0100

    Added files initially

---
+++ dScriptModule/test/test_dScriptModule.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# version: 2018.10.05
+# description: script to test the dScriptServer and corresponding classes
+
+import os
+import sys
+import logging
+import time
+
+def main():
+    logging.info("main: starting program...")
+    import dScriptModule
+
+    logging.debug("main: create dScriptServer object")
+    server = dScriptModule.ScriptServer()
+    
+    logging.debug("main: starting server")
+    server.StartServer()
+
+    logging.debug("main: sleeping until interrupt")
+    try:
+        while True:
+            time.sleep(3600)
+    except KeyboardInterrupt:
+        pass
+    logging.debug("main: keyboard interrupted")
+
+    #logging.debug("main: sleeping 10 seconds") 
+    #time.sleep(10)
+    logging.debug("main: stopping server")
+    server.StopServer()
+    #logging.debug("main: starting server")
+    #server.StartServer()
+
+    logging.debug("main: exit now - BYE BYE")
+    sys.exit()
+
+#logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+main()
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import dScriptModule
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dScriptModule',
+    version=dScriptModule.__version__,
+    url='https://github.com/mk-maddin/dScriptModule',
+    author='Martin Kraemer',
+    author_email='mk.maddin@gmail.com',
+    description='Home Assistant ready python module for dScript boards (by Robot-Electronics / Devantech Ltd.) with CUSTOM app firmware'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
